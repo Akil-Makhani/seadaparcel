8d26f8f30ee2fd2d84baae07ab221a4f6ad6a047
Akil Makhani <akilmakhani7@gmail.com>
2025-09-23 12:49:28 +0530

    header logo fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,10 @@
 import { Link } from "react-router-dom";
-import toast from "react-hot-toast";
 
 export default function Header() {
   return (
     <header className='sticky top-0 z-30 backdrop-blur bg-white/70 border-b border-slate-100'>
       <div className='container-section flex h-20 items-center justify-between'>
-        <div className='flex items-center gap-3'>
+        <div className='-ml-16 flex items-center gap-3'>
           <Link to='/' className='inline-flex items-center'>
             <img
               src='/images/logo_4.png'
